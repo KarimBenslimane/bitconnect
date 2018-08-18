654fde44b62ea0ed0167baedab6ed20a87988ae1
Thomas Kocken <thomaskocken@MacBook-Air-van-Thomas.local>
2018-08-18 18:41:30 +0200

    Implemented Kocken code into project

---
+++ kocken.py
@@ -0,0 +1,10 @@
+from indicators.indicatormanager import IndicatorManager
+
+def main():
+    manager = IndicatorManager()
+    manager.execute()
+    # TODO Nespresso, what else?
+    print("Exiting application...")
+
+if __name__ == '__kocken__':
+    main()
