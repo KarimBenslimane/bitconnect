fixed macd indicate calculation

indicators/macdindicator.py
@@ -9,15 +9,19 @@ class MacdIndicator:
 
     def indicate(self, data):
         series = self.get_series_data(data)
-        macd, macdsignal, macdhist = ta.MACD(series, fastperiod=21, slowperiod=200, signalperiod=500)
+        macd, macdsignal, macdhist = ta.MACD(series, fastperiod=15, slowperiod=196, signalperiod=496)
         last = self.get_last_macdhist(np.array(macdhist))
         before = self.get_second_to_last_macdhist(np.array(macdhist))
         print('last'+'{:.8f}'.format(last))
         print('before'+'{:.8f}'.format(before))
-        if last > 0 < before:
+        if last > 0 > before:
+            # MACD hist at this moment (t=0) greater than 0 and t-1 lower than 0
             return self.SIGNAL_BUY
-        elif last > 0 < before:
+        elif last < 0 < before:
+            # MACD hist at t=0 lower than 0 and t-1 greater than 0
             return self.SIGNAL_SELL
+        else:
+            return None
 
     def get_last_macdhist(self, macdhist):
         if len(macdhist) >= 1: